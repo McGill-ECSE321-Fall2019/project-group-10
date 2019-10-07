a560b062ea561b45f31e90dd91ccabe7853afe17
Yoan  Polumarc'k <yoanpolumarck@Yoans-Computer.local>
2019-10-07 11:28:38 -0400

    Initiated spring project and configured gradle

---
+++ src/main/java/group10/ProjectGroup10Application.java
@@ -0,0 +1,21 @@
+package group10;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class ProjectGroup10Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectGroup10Application.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+
+}
---
+++ src/test/java/group10/ProjectGroup10ApplicationTests.java
@@ -0,0 +1,16 @@
+package group10;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectGroup10ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
