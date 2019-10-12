c082a28a13fddd4f6ebe7837c4893237aa258ff6
taylorlynn <taylor.curtis@mail.mcgill.ca>
2019-10-12 15:03:10 -0400

    Fixed Tutor

--- Project-Backend/src/test/java/ca/mcgill/ecse321/project/service/AvailabilityTest.java
+++ Project-Backend/src/test/java/ca/mcgill/ecse321/project/service/AvailabilityTest.java
@@ -3,7 +3,7 @@ package ca.mcgill.ecse321.project.service;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
