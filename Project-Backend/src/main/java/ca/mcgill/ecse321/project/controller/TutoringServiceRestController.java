34becf046c12da3a75ec54775f432d3c57f2f2fc
ChelseaM-C <chelsea.myers-colet@mail.mcgill.ca>
2019-10-19 18:59:14 -0400

    Update TutoringServiceRestController.java

--- Project-Backend/src/main/java/ca/mcgill/ecse321/project/controller/TutoringServiceRestController.java
+++ Project-Backend/src/main/java/ca/mcgill/ecse321/project/controller/TutoringServiceRestController.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.eventregistration.dto.EventDto;
+import ca.mcgill.ecse321.eventregistration.dto.PersonDto;
+import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Person;
 import ca.mcgill.ecse321.project.dto.*;
 import ca.mcgill.ecse321.project.model.*;
 import ca.mcgill.ecse321.project.service.*;
@@ -22,5 +26,43 @@ import ca.mcgill.ecse321.project.service.*;
 @CrossOrigin(origins = "*")
 @RestController
 public class TutoringServiceRestController {
+	
+	@Autowired
+	TutoringAppService service;
+	
+	// Get all the schools offered by the application
+	@GetMapping(value = { "/events", "/events/" })
+	public List<UniversityDTO> getAllUniversities() {
+		List<UniversityDTO> universityDtos = new ArrayList<>();
+		
+		// get universities from the tutoring service
+		for (University university : service.getAllUniversities()) {
+			// convert model class to a data transfer object
+			universityDtos.add(convertToDto(university));
+		}
+		return universityDtos;
+	}
+	
+	// Get all the schools offered by the application
+	@GetMapping(value = { "/events", "/events/" })
+	public List<UniversityDTO> getAllUniversities() {
+		List<UniversityDTO> universityDtos = new ArrayList<>();
+		
+		// get universities from the tutoring service
+		for (University university : service.getAllUniversities()) {
+			// convert model class to a data transfer object
+			universityDtos.add(convertToDto(university));
+		}
+		return universityDtos;
+	}
+	
+	// Convert the model university to a DTO object
+	private UniversityDTO convertToDto(University u) {
+		if (u == null) {
+			throw new IllegalArgumentException("There is no such Person!");
+		}
+		UniversityDTO uDTO = new UniversityDTO(u.getName(), u.getAddress());
+		return uDTO;
+	}
 
 }
