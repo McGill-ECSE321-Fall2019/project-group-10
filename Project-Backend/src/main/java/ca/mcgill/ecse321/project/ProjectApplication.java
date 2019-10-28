9b0b5d3e9970a98bb089b803df293d598df6f5e0
Cpt0Teemo <ypoulmarck@gmail.com>
2019-10-28 10:36:49 -0400

    Reverted changes to main class

--- Project-Backend/src/main/java/ca/mcgill/ecse321/project/ProjectApplication.java
+++ Project-Backend/src/main/java/ca/mcgill/ecse321/project/ProjectApplication.java
@@ -1,12 +1,9 @@
 package ca.mcgill.ecse321.project;
 
-import ca.mcgill.ecse321.project.model.Session;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.sql.Time;
 
 @RestController
 @SpringBootApplication
@@ -18,12 +15,7 @@ public class ProjectApplication {
 
   @RequestMapping("/")
   public String greeting(){
-    try{
-      Session session = new Session();
-      session.setTime(new Time(100));
-      JavaEmail.NotifyTutor("ypoulmarck@gmail.com", session);
-    }catch (Exception e){ return "Fail" + e.getMessage() + e.getStackTrace();}
-    return "Pass";
+    return "Hello world!";
   }
 
 }
