8de2996f09cc70c5f52f3435cc080bef20c84cc5
ChelseaM-C <chelsea.myers-colet@mail.mcgill.ca>
2019-10-30 20:27:25 -0400

    Update TutoringServiceRestController.java

--- Project-Backend/src/main/java/ca/mcgill/ecse321/project/controller/TutoringServiceRestController.java
+++ Project-Backend/src/main/java/ca/mcgill/ecse321/project/controller/TutoringServiceRestController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//import ca.mcgill.ecse321.project.JavaEmail;
+import ca.mcgill.ecse321.project.JavaEmail;
 import ca.mcgill.ecse321.project.ErrorStrings;
 import ca.mcgill.ecse321.project.dto.*;
 import ca.mcgill.ecse321.project.model.*;
