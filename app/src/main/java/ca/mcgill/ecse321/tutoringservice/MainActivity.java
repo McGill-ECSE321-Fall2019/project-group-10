b20e7d1d6376acf53d646d4889a10875d979e513
alexander <alexander@MacBook-Pro-4.local>
2019-11-23 13:13:39 -0500

    Sign up page configured

--- app/src/main/java/ca/mcgill/ecse321/tutoringservice/MainActivity.java
+++ app/src/main/java/ca/mcgill/ecse321/tutoringservice/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+ 
     public void goToDashBoard (View v) {
 
         error = "";
