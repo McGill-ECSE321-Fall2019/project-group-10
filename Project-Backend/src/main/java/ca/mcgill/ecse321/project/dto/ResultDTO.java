19beaa38d4ca535599c5c5eb2a9bce06027485b8
ChelseaM-C <chelsea.myers-colet@mail.mcgill.ca>
2019-11-26 16:23:41 -0500

    Add login2 to return JSON

---
+++ Project-Backend/src/main/java/ca/mcgill/ecse321/project/dto/ResultDTO.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.project.dto;
+
+public class ResultDTO {
+	
+	private boolean result;
+	
+	public ResultDTO(boolean result) {
+		this.result = result;
+	}
+	
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+	
+	public boolean getResult() {
+		return this.result;
+	}
+
+}
